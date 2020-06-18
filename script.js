a8b6ffbd5714b9a14a4e8929a629344e9d3a464c
unknown <katharina.r@hotmail.fr>
2020-06-18 14:29:53 +0200

    directions

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ window.onload = function() {
     canvas.style.border = "1px solid";
     document.body.appendChild(canvas);
     ctx = canvas.getContext('2d');
-    george = new Snake([[6,4], [5,4], [4,4]]);
+    george = new Snake([[6,4], [5,4], [4,4]], "right");
     refreshCanvas();
     
     }
@@ -36,8 +36,9 @@ window.onload = function() {
         ctx.fillRect(x,y, blockSize, blockSize);
     }
 
-    function Snake(body) {
+    function Snake(body, direction) {
         this.body = body;
+        this.direction = direction;
         this.draw = function() {
             ctx.save();
             ctx.fillStyle = "#ff0000";
@@ -50,11 +51,73 @@ window.onload = function() {
         this.advance = function() {
 
             var nextPosition = this.body[0].slice();
-            nextPosition[0] += 1;
+
+            switch(this.direction) {
+                case "left":
+                    nextPosition[0] -= 1;
+                    break;
+                case "right":
+                    nextPosition[0] += 1;
+                    break;
+                case "down":
+                    nextPosition[1] += 1;
+                    break;
+                case "up":
+                    nextPosition[1] -= 1;
+                    break;
+                default:
+                    throw("Invalid Direction");
+            }
+
             this.body.unshift(nextPosition);
             this.body.pop();
 
+        };
+
+        this.setDirection = function(newDirection) {
+            
+            var allowedDirections;
+            switch(this.direction) {
+                case "left":
+                case "right":
+                    allowedDirections = ["up", "down"];
+                    break;
+                case "down":
+                case "up":
+                    allowedDirections = ["left", "right"]
+                    break;
+                default:
+                    return;
+            }
+            if(allowedDirections.indexOf(newDirection) > -1) {
+                this.direction = newDirection;
+            }
 
         };
     }
+
+    document.onkeydown = function handleKeyDown(e) {
+
+        var key = e.keyCode;
+        var newDirection;
+        switch(key) {
+            case 37:
+                newDirection = "left";
+                break;
+            case 38:
+                newDirection = "up";
+                break;
+            case 39:
+                newDirection = "right";
+                break;
+            case 40:
+                newDirection = "down";
+                break;
+            default:
+                return;
+        }
+        george.setDirection(newDirection);
+
+    }
+
 }
